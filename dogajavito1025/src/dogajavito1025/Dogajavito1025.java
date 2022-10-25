7c813ee496bcdcf0b6b3edfed5d001958e46c4dc
Domek1232 <83282741+Domek1232@users.noreply.github.com>
2022-10-25 10:27:05 +0200

    dolgozatfeladat

---
+++ dogajavito1025/src/dogajavito1025/Dogajavito1025.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dogajavito1025;
+
+/**
+ *
+ * @author user3
+ */
+public class Dogajavito1025 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String szoveg = "Sok lúd dosznót győz";
+        String masikszoveg = "Sok kacsa malacot nyer";
+        //1.FELADAT
+        System.out.println(szoveg.length());
+        //2.FELADAT
+        System.out.println(szoveg.indexOf(0));
+        //3.FELADAT
+        System.out.println(szoveg.compareTo(masikszoveg));
+        //4.FELADAT
+        System.out.println(szoveg.contains("lúd"));
+        //5.FELADAT
+        System.out.println(szoveg.indexOf("lúd"));
+        //6.FELADAT
+        System.out.println(szoveg.replace("lúd", "veréb"));
+        //7.FELADAT
+        System.out.println();
+        
+    }
+    
+}
