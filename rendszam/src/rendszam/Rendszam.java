ca0b9c7a272bcfc490e7760c7149b5382561e885
Domek1232 <83282741+Domek1232@users.noreply.github.com>
2022-11-08 11:07:10 +0100

    rendszam

--- rendszam/src/rendszam/Rendszam.java
+++ rendszam/src/rendszam/Rendszam.java
@@ -5,6 +5,10 @@
  */
 package rendszam;
 
+import static java.lang.Character.isDigit;
+import static java.lang.Character.isLetter;
+import java.util.Scanner;
+
 /**
  *
  * @author user3
@@ -15,7 +19,23 @@ public class Rendszam {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner bill = new Scanner(System.in);
+        String rendszam = new String();
+        
+        do{
+            System.out.println("Add meg a rendszáot: ");
+            rendszam = bill.nextLine();
+        } while (!isValidRendszamV2(rendszam));
+    }
+
+    private static boolean isValidRendszamV2(String rsz) {
+        if i;
+        if (rsz.length() == 6 && isDigit(rsz.charAt(3)) && isDigit(rsz.charAt(4)) && isDigit(rsz.charAt(5))
+                && isLetter(rsz.charAt(0)) && isLetter(rsz.charAt(1)) && isLetter(rsz.charAt(2))){
+            return true;
+        }
+        
+            return false;
     }
     
 }
